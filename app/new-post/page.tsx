c92edb8e2272906f91b75fb16993266fec15e27a
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2024-10-07 15:25:16 +0700

    chore: separate server action at actions folder

--- app/new-post/page.tsx
+++ app/new-post/page.tsx
@@ -1,3 +1,4 @@
+import { createPost } from "@/actions/posts";
 import FormSubmit from "@/components/form-submit";
 import PostForm from "@/components/post-form";
 import { storePost } from "@/lib/posts";
@@ -5,42 +6,6 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const NewPostPage = () => {
-  async function createPost(prevState:any, formData: FormData){
-    "use server";
-    const title = formData.get("title") as string;
-    const image = formData.get("image") as File;
-    const content = formData.get("content") as string;
-
-    let errors: string[] = [];
-
-    console.log("image", image);
-    if (!title || title.trim().length === 0) {
-      errors.push("Title is required");
-    }
-
-    if (!content || content.trim().length === 0) {
-      errors.push("Content is required");
-    }
-
-    if (!image || image.size === 0) {
-      errors.push("Image is required");
-    }
-
-    if (errors.length > 0) {
-      return {
-        errors,
-      };
-    }
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed");
-  }
   return <PostForm action={createPost} />;
 };
 
